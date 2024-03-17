20a136d52265ca657462ea77559587303b737c8c
Wwestfall91 <wwestfall91@gmail.com>
2024-03-16 22:47:51 -0400

    Add thumbnail component

---
+++ src/thumbnail/Thumbnail.tsx
@@ -0,0 +1,14 @@
+import "./Thumbnail.css";
+
+function Thumbnail() {
+  return (
+    <div className="thumbnail-container">
+      <img
+        className="thumbnail-image"
+        src="https://i.ytimg.com/vi/3zeHBreluF8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBxj8WBlrzObI-XmD4bhjf8vWnbtQ"
+      ></img>
+    </div>
+  );
+}
+
+export default Thumbnail;
--- src/video-grid/VideoGrid.tsx
+++ src/video-grid/VideoGrid.tsx
@@ -1,18 +1,24 @@
+import Thumbnail from "../thumbnail/Thumbnail";
 import "./VideoGrid.css";
 
 function VideoGrid() {
   return (
     <div className="video-grid-container">
-      <div className="video-grid-item"></div>
-      <div className="video-grid-item"></div>
-      <div className="video-grid-item"></div>
-      <div className="video-grid-item"></div>
-      <div className="video-grid-item"></div>
-      <div className="video-grid-item"></div>
-      <div className="video-grid-item"></div>
-      <div className="video-grid-item"></div>
-      <div className="video-grid-item"></div>
-      <div className="video-grid-item"></div>
+      <div className="video-grid-item">
+        <Thumbnail></Thumbnail>
+      </div>
+      <div className="video-grid-item">
+        <Thumbnail></Thumbnail>
+      </div>
+      <div className="video-grid-item">
+        <Thumbnail></Thumbnail>
+      </div>
+      <div className="video-grid-item">
+        <Thumbnail></Thumbnail>
+      </div>
+      <div className="video-grid-item">
+        <Thumbnail></Thumbnail>
+      </div>
     </div>
   );
 }
